fa3b6a0dd45f33eb8247a025dd7b08ae559f1b56
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-17 10:46:31 -0500

    clase deportista

---
+++ deportista.py
@@ -0,0 +1,19 @@
+from persona import Persona
+
+class Deportista(Persona):
+  def__init__(self, nombre, edad, altura, sexo, deporte, añosPracticando):
+    super().__init__(nombre, edad, altura, sexo)
+    self._deporte = deporte
+    self._añosPracticando = añosPracticando
+
+  def getDeporte(self):
+        return self._deporte
+
+    def setDeporte(self, deporte):
+        self._deporte = deporte
+
+    def getAñosPracticando(self):
+        return self._añosPracticando
+
+    def setAñosPracticando(self, añosPracticando):
+        self._añosPracticando = añosPracticando
